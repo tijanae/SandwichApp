904f41fb44db76beeae5cc99b30434bba5eb8396
Tia Lendor <tialendor@pursuit.org>
2023-05-30 08:36:11 -0400

    Initial Commit

---
+++ SandwichApp/SandwichAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandwichAppApp.swift
+//  SandwichApp
+//
+//  Created by Tia Lendor on 5/30/23.
+//
+
+import SwiftUI
+
+@main
+struct SandwichAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
